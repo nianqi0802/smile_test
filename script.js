5fd1eb49f8b3b37bfb0ae310031f714cf602b97a
Nianqi Zhang <42988173+nianqi0802@users.noreply.github.com>
2021-01-28 23:33:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ Promise.all([
 ]).then(startVideo)
 
 function startVideo() {
-  navigator.mediaDevices.getUserMedia(
+  navigator.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)

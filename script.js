0acc6338607b5156f34c9aaa7e88819ad3800a52
Nianqi Zhang <42988173+nianqi0802@users.noreply.github.com>
2021-01-28 23:48:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,23 +13,15 @@ Promise.all([
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
 
-// function startVideo() {
-//   navigator.getUserMedia(
-//     { video: {} },
-//     stream => video.srcObject = stream,
-//     err => console.error(err)
-//   )
-// }
-function startVideo(){
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  // Not adding `{ audio: true }` since we only want video now
-  navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-    //video.src = window.URL.createObjectURL(stream);
-    video.srcObject = stream;
-    video.play();
-  });
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
 }
-}
+
+
 
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
